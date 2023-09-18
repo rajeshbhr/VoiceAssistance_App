2470a5bfcdaf5fcdc4adf50990acdaff234a58ba
rajeshbhr <rajeshbehera69698@gmail.com>
2023-09-18 20:08:43 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/voiceassistance_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.voiceassistance_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+
+import 'package:flutter/material.dart';
+import 'package:voiceassistance_app/pallete.dart';
+
+import 'home_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Allen',
+      theme: ThemeData.light(useMaterial3: true).copyWith(
+        scaffoldBackgroundColor: Pallete.whiteColor,
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Pallete.whiteColor,
+        ),
+      ),
+      home: const HomePage(),
+    );
+  }
+}
